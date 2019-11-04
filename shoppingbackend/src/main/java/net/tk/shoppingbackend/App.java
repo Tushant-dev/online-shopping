56e3f7cbaf7503eddece0fee81bc2b36024dd82f
Tushant Singh <tushant.kumar.singh@gmail.com>
2019-11-05 03:45:43 +0530

    my project setup completed Successfully.

---
+++ shoppingbackend/src/main/java/net/tk/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.tk.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
